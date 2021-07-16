60d41e411d22bd9830ead7dccad2c20b376dd856
khatv1508 <vinhkha1508@gmail.com>
2021-07-16 12:06:03 +0700

    vinhkha commit 16/07/2021 update style.css, main.js, index.html add image

--- project_market/js/main.js
+++ project_market/js/main.js
@@ -5,11 +5,11 @@
             initialRating: 4,
             strokeColor: '#894A00',
             strokeWidth: 10,
-            starSize: 25,
+            starSize: 20,
             emptyColor: 'lightgray',
             hoverColor: 'orange',
             activeColor: 'gold',
-            ratedColors: 'gold',
+            ratedColors: ['#FDCC0D', '#FDCC0D', '#FDCC0D', '#FDCC0D', '#FDCC0D'],
             starShape: 'straight'
         });
     });
